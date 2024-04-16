6bab363374a9ea917ab31aa8dec23ca6d0f3f23f
ikmrn <ibragimovkmrn@gmail.com>
2024-04-16 23:16:36 +0500

    test: Test remove from minHeap

--- priority-queue/Main.java
+++ priority-queue/Main.java
@@ -1,7 +1,7 @@
 
 public class Main{
   public static void main(String[] args) {
-    PriorityQueueMax minHeap = new PriorityQueueMax(5);
+    PriorityQueueMin minHeap = new PriorityQueueMin(5);
     minHeap.insert(5);
     minHeap.insert(3);
     minHeap.insert(7);
@@ -11,6 +11,9 @@ public class Main{
     minHeap.insert(1);
     minHeap.insert(4);
     minHeap.insert(-2);
+    System.out.println(minHeap.remove());
+    System.out.println(minHeap.remove());
+    System.out.println(minHeap.remove());
     System.out.println(minHeap.remove());
     System.out.println(minHeap);
   
