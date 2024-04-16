c904e50276074a2d95094298ec0d26e588d732f2
ikmrn <ibragimovkmrn@gmail.com>
2024-04-16 23:10:48 +0500

    feat: Initial commit

---
+++ priority-queue/PriorityQueueMin.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class PriorityQueueMin{
+  private int length = 0;
+  private int capacity;
+  private int[] arr;
+
+  public PriorityQueueMin(int capacity){
+    this.capacity = capacity;
+    this.arr = new int[this.capacity];
+  }
+
+  
+
+}
