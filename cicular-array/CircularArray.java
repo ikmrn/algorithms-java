3f3121ba74a28f58c5190365d96dd2ac6612ae69
ikmrn <ibragimovkmrn@gmail.com>
2024-04-16 18:13:33 +0500

    feat: Add circular enqueue, dequeue

--- cicular-array/CircularArray.java
+++ cicular-array/CircularArray.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class CircularArray{
   private int[] items;
   private int capacity;
   public int length = 0;
+  private int head = 0, tail = 0;
 
   public CircularArray(int capacity){
     this.items = new int[capacity];
@@ -9,6 +12,47 @@ public class CircularArray{
   }
 
   public void enqueue(int item){
-    
+    if (this.length == this.capacity){
+      growArray();
+    }
+    items[this.tail++ % this.capacity] = item;
+    this.length++;
+  }
+
+  public int dequeue(){
+    if (this.length == 0){
+      throw new IllegalStateException("The queue is empty");
+    }
+    int item = items[this.head % this.capacity];
+    items[this.head % this.capacity] = 0;
+    this.head++;
+    this.length--;
+    return item;
+  }
+
+  public void growArray(){
+    this.capacity *= 2;
+    int[] newArray = new int[this.capacity];
+    for (int i = 0; i < this.length; ++i){
+      newArray[i] = items[(i + this.head) % this.length];
+    }
+    items = newArray;
+    this.head = 0;
+    this.tail = this.length;
+  }
+
+  public int peek(){
+    if (this.length == 0){
+      throw new IllegalStateException("The queue is empty");
+    }
+    return items[this.head];
+  }
+  public boolean isEmpty(){
+    return this.length == 0;
+  }
+
+  @Override
+  public String toString(){
+    return Arrays.toString(this.items);
   }
 }
--- cicular-array/Main.java
+++ cicular-array/Main.java
@@ -1,16 +1,26 @@
 
 public class Main{
   public static void main(String[] args){
-    ArrayQueue queue = new ArrayQueue(5);
+    CircularArray queue = new CircularArray(5);
     queue.enqueue(10);
     queue.enqueue(20);
     queue.enqueue(30);
-    queue.enqueue(30);
-    queue.enqueue(20);
     queue.enqueue(23);
     System.out.println(queue);
+    System.out.println(queue);
     var front = queue.dequeue();
-    System.out.println(front);
+    front = queue.dequeue();
+    front = queue.dequeue();
+    queue.enqueue(24);
+    queue.enqueue(25);
+    queue.enqueue(26);
+    queue.enqueue(27);
+    queue.enqueue(28);
+    queue.enqueue(29);
+    
+    
+
+
     System.out.println(queue);
 
 
