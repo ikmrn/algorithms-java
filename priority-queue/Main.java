c7cae5e2c949fe874363483acdbbd8a90955f807
ikmrn <ibragimovkmrn@gmail.com>
2024-04-16 22:54:02 +0500

    feat: Initial commit

--- priority-queue/Main.java
+++ priority-queue/Main.java
@@ -1,5 +1,19 @@
-public class Main{
-  public static void main() {
 
+public class Main{
+  public static void main(String[] args) {
+    PriorityQueueMax minHeap = new PriorityQueueMax(5);
+    minHeap.insert(5);
+    minHeap.insert(3);
+    minHeap.insert(7);
+    minHeap.insert(1);
+    minHeap.insert(5);
+    minHeap.insert(0);
+    minHeap.insert(1);
+    minHeap.insert(4);
+    minHeap.insert(-2);
+    System.out.println(minHeap.remove());
+    System.out.println(minHeap);
+  
+    ;
   }
 }
