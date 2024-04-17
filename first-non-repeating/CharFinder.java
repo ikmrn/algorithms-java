fe7141faef5632205b55e28d2c83bc08e278ff57
ikmrn <ibragimovkmrn@gmail.com>
2024-04-17 15:19:59 +0500

    feat: Find first non-repeating character

--- first-non-repeating/CharFinder.java
+++ first-non-repeating/CharFinder.java
@@ -1,5 +1,33 @@
 public class CharFinder{
-  public int findFirstNonRepeatingChar(String str){
-    
+  public char findFirstNonRepeatingChar(String str){
+    int[] hashArray = new int[52];
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (Character.isAlphabetic(ch)) {
+        int hasValue = getHashValue(ch);
+        hashArray[hasValue]++;
+      }
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (Character.isAlphabetic(ch)) {
+        int hasValue = getHashValue(ch);
+        if (hashArray[hasValue] == 1){
+          return ch;
+        }
+      }
+    }
+    return '~';
   }
+
+  private int getHashValue(char ch) {
+    if (Character.isUpperCase(ch)) {
+        return ch - 'A'; // A-Z mapped to 0-25
+    } else {
+        return ch - 'a' + 26; // a-z mapped to 26-51
+    }
+  }
+
 }
--- first-non-repeating/Main.java
+++ first-non-repeating/Main.java
@@ -1,5 +1,14 @@
 public class Main{
   public static void main(String[] args){
-    
+    String greenApple = "A Green Apple";
+    CharFinder finder = new CharFinder();
+    char character = finder.findFirstNonRepeatingChar(greenApple);
+    System.out.println(character);
+    String repeat = "AABJBJGGASASKFFK";
+    character = finder.findFirstNonRepeatingChar(repeat);
+    System.out.println(character);
+    String last = "A Green AppleA Green Applef";
+    character = finder.findFirstNonRepeatingChar(last);
+    System.out.println(character);
   }
 }
