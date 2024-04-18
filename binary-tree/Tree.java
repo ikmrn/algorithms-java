6de20ce6c2d82a504a09cb628714d33281ac77c5
ikmrn <ibragimovkmrn@gmail.com>
2024-04-18 08:52:02 +0500

    feat: Add method to insert a TreeNode

--- binary-tree/Tree.java
+++ binary-tree/Tree.java
@@ -1,6 +1,29 @@
 public class Tree {
-  
+  TreeNode root;
 
+  public void insert(int value) {
+    TreeNode node = new TreeNode(value);
+    if (root == null) {
+      root = node;
+    }
+    TreeNode curr = root;
+    while (true) {
+      if (value < curr.value) {
+        if (curr.leftChild == null) {
+          curr.leftChild = node;
+        }
+        curr = curr.leftChild;
+      } else {
+        if (curr.rightChild == null) {
+          curr.rightChild = node;
+        }
+        curr = curr.rightChild;
+      }
+    }
+
+    p
+
+  }
 
   private class TreeNode {
     int value;
