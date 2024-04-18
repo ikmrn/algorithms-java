ada61a750241ff198552e7a38cfe733e962373be
ikmrn <ibragimovkmrn@gmail.com>
2024-04-18 09:28:15 +0500

    fix: Break out of the loop when child is null

--- binary-tree/Tree.java
+++ binary-tree/Tree.java
@@ -5,24 +5,24 @@ public class Tree {
     TreeNode node = new TreeNode(value);
     if (root == null) {
       root = node;
+      return;
     }
     TreeNode curr = root;
     while (true) {
       if (value < curr.value) {
         if (curr.leftChild == null) {
           curr.leftChild = node;
+          break;
         }
         curr = curr.leftChild;
       } else {
         if (curr.rightChild == null) {
           curr.rightChild = node;
+          break;
         }
         curr = curr.rightChild;
       }
     }
-
-    p
-
   }
 
   private class TreeNode {
