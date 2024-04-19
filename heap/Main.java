b36bd75da856aa19e26722751d34972a349f1f67
ikmrn <ibragimovkmrn@gmail.com>
2024-04-19 19:40:55 +0500

    feat: Initial commit

---
+++ heap/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+  public static void main(String[] args) {
+    MaxHeap heap = new MaxHeap(5);
+  }
+}
---
+++ heap/MaxHeap.java
@@ -0,0 +1,15 @@
+public class MaxHeap {
+  int[] arr;
+
+  public MaxHeap(int capacity) {
+    this.arr = new int[capacity];
+  }
+
+  public void insert(int value) {
+
+  }
+
+  private void growArray() {
+
+  }
+}
