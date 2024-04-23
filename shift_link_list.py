7ce132c999440de4bbf5e8382f31f7f7563ead5e
ikmrn <ibragimovkmrn@gmail.com>
2024-04-23 12:10:28 +0500

    fix: Wrap k around the list if it is larger

--- shift_link_list.py
+++ shift_link_list.py
@@ -1,41 +1,40 @@
+# This is the class of the input linked list.
 class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-head = LinkedList(0)
-head.next = LinkedList(1)
-head.next.next = LinkedList(2)
-head.next.next.next = LinkedList(3)
-head.next.next.next.next = LinkedList(4)
-head.next.next.next.next.next = LinkedList(5)
-
-curr = head
-while curr:
-    print(curr.value)
-    curr = curr.next
+def get_linked_list_len(head):
+    count = 0
+    curr = head
+    while curr:
+        count += 1
+        curr = curr.next
+    return count
 
 
 def shiftLinkedList(head, k):
+    if k == 0:
+        return head
+    list_len = get_linked_list_len(head)
+    count = k % list_len
+    if count < 0:
+        count = list_len + count
+    if count == 0:
+        return head
     curr = head
     k_node_prev = head
-    count = 0
     while curr.next:
-        if count < k + 1:
-            count += 1
+        if count > 0:
+            count -= 1
             curr = curr.next
             continue
 
-        k_node = k_node_prev.next
+        k_node_prev = k_node_prev.next
         curr = curr.next
-    if count != k:
-        return
     k_node = k_node_prev.next
     k_node_prev.next = None
-
-    print(k_node.value)
-    print(curr.value)
-
-
-shiftLinkedList(head, 2)
+    curr.next = head
+    head = k_node
+    return head
